a9fcc39f0e34dba91b218082af814a237e7a5658
Dani-Torrev <danieltv0212@gmail.com>
2026-04-13 13:15:17 +0200

    Add files via upload

---
+++ Proyecto MP/salas.h
@@ -0,0 +1,17 @@
+#ifndef SALAS_H
+#define SALAS_H
+
+// Estructura para las salas 
+typedef struct {
+    char id_sala[3];       
+    char nombre[31];       
+    char tipo[10];         
+    char descripcion[151]; 
+} Sala;
+
+// Prototipos de funciones
+void imprimir_datos_sala(Sala s);
+int es_sala_final(Sala s);
+Sala* cargar_salas(int *num_salas);
+
+#endif
