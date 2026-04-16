a4a786b9b4f94bb9defe861342cdcee7ca919c07
_mariooloopez_ <Mariolophid@hotmail.com>
2026-04-16 15:40:14 +0200

    Add files via upload

--- Modulos Pablo/puzles.h
+++ Modulos Pablo/puzles.h
@@ -15,6 +15,20 @@ typedef struct {
 
 struct EstadoJuego_s;
 
+/**
+ * Función: resolver_puzzle_actual
+ * Descripción: Resuelve el puzzle en la sala actual del juego
+ * 
+ * Precondiciones:
+ *   - juego debe ser un puntero válido a una estructura EstadoJuego_s
+ *   - El estado del juego debe contener una sala actual válida
+ *   - Debe existir un puzzle asociado a la sala actual
+ * 
+ * Postcondiciones:
+ *   - Se actualiza el estado del puzzle correspondiente en juego
+ *   - El campo resuelto del puzzle se marca según el resultado
+ *   - El estado del juego puede modificarse para reflejar el avance
+ */
 void resolver_puzzle_actual(struct EstadoJuego_s *juego);
 
 #endif
