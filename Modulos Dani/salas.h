1bfc919215a65154f48dece37b085d01aa9b1ddd
_mariooloopez_ <Mariolophid@hotmail.com>
2026-04-16 15:25:11 +0200

    Add files via upload

--- Modulos Dani/salas.h
+++ Modulos Dani/salas.h
@@ -10,7 +10,31 @@ typedef struct {
 } Sala;
 
 // Prototipos de funciones
+
+/**
+ * Función: imprimir_datos_sala
+ * Descripción: Imprime la información de una sala
+ * 
+ * Precondiciones:
+ *   - s debe ser una estructura Sala válida
+ * 
+ * Postcondiciones:
+ *   - Se muestra por pantalla la información contenida en s
+ *   - La estructura s no se modifica
+ */
 void imprimir_datos_sala(Sala s);
+
+/**
+ * Función: es_sala_final
+ * Descripción: Comprueba si una sala es de tipo final
+ * 
+ * Precondiciones:
+ *   - s debe ser una estructura Sala válida
+ * 
+ * Postcondiciones:
+ *   - Devuelve 1 si s es una sala final, 0 en caso contrario
+ *   - La estructura s no se modifica
+ */
 int es_sala_final(Sala s);
 
 #endif
