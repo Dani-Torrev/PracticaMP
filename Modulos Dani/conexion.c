cd997627678b30f083023714b554e21ee247d0d5
_mariooloopez_ <Mariolophid@hotmail.com>
2026-04-16 17:07:34 +0200

    Add files via upload

--- Modulos Dani/conexion.c
+++ Modulos Dani/conexion.c
@@ -1,35 +1,37 @@
+// Includes para entrada/salida, strings y header de conexiones
 #include <stdio.h>
 #include <string.h>
 #include "conexion.h"
 
-// Comprueba si el estado de la conexión es "Activa"
+// Función para comprobar si el estado de la conexión es "Activa"
 int esta_abierta(Conexion c) {
     // strcmp devuelve 0 si las cadenas son iguales
     if (strcmp(c.estado, "Activa") == 0) {
-        return 1; 
+        return 1; // Está abierta
     }
-    return 0; 
+    return 0; // No está abierta
 }
-// Cambia el estado de la conexión mediante un puntero
+
+// Función para cambiar el estado de la conexión a "Activa" mediante un puntero
 void abrir_paso(Conexion *c) {
     if (c != NULL) {
-        strcpy(c->estado, "Activa");
+        strcpy(c->estado, "Activa"); // Cambiamos el estado
         printf("[INFO] La conexion %s ahora esta Activa.\n", c->id_conexion);
     }
 }
 
-// Compara si la condicion de la conexion coincide con un ID de objeto
+// Función para comparar si la condición de la conexión coincide con un ID de objeto
 int requiere_objeto(Conexion c, char* id_objeto) {
     if (strcmp(c.condicion, id_objeto) == 0) {
-        return 1;
+        return 1; // Requiere ese objeto
     }
-    return 0; 
+    return 0; // No requiere ese objeto
 }
 
-// Compara si la condicion coincide con un ID de puzzle
+// Función para comparar si la condición coincide con un ID de puzzle
 int tiene_puzzle(Conexion c, char* id_puzzle) {
     if (strcmp(c.condicion, id_puzzle) == 0) {
-        return 1; 
+        return 1; // Tiene ese puzzle
     }
-    return 0; 
+    return 0; // No tiene ese puzzle
 }
